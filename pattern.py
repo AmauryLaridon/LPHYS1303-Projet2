7aa5917d7f18c79b15d8adc0f7526cb0a0da51cb
Amaury Laridon <amaurylaridon@protonmail.com>
2021-04-30 19:37:35 +0200

    amélioration graphique

--- pattern.py
+++ pattern.py
@@ -6,7 +6,7 @@ import scipy.signal as scsg
 
 
 r = 0.2
-L = 100
+L = 200
 
 
 def u_0(x):
@@ -14,7 +14,7 @@ def u_0(x):
 
 def v_0(x):
     return (0.5 - 0.25*cos(2*pi*x/L))
-    
+
 def w_0(x):
     return exp(-(x-L/2)**2)
 
@@ -72,10 +72,10 @@ def SH(f0):
         u = np.real(np.fft.ifft(np.fft.ifftshift(N*uk)))
         U[:,t] = u
         ukc = (1/N)*np.fft.fftshift(np.fft.fft(u**3))
-        
+
         """pc1 = (1/(2*pi))*np.convolve(uk,uk, mode = "same")
         pc2 = (1/(2*pi))*np.convolve(pc1,uk, mode = "same")"""
-        
+
         """pc1 = (1/(2*pi))*scsg.fftconvolve(uk,uk, mode = "same")
         pc2 = (1/(2*pi))*scsg.fftconvolve(pc1,uk, mode = "same")"""
         DFTc[:,t] = ukc
@@ -87,19 +87,24 @@ time_ev = SH(u_0)
 U = time_ev[0]
 D = time_ev[1]
 
-plt.plot(x_range, U[:,0], label = "t = 0")
-plt.plot(x_range, U[:,1], label = "t = {}".format(dt))
-plt.plot(x_range, U[:,-1], label = "dernier temps")
+
+plt.plot(x_range, U[:,0], label = "t = 0s")
+plt.plot(x_range, U[:,1], label = "t = {}s".format(dt))
+plt.plot(x_range, U[:,int(T/4)], label = "t = {:2.2f}s".format((T/4)*dt))
+plt.plot(x_range, U[:,-1], label = "t = {:2.2f}s".format(t_max))
+plt.xlabel("x")
+plt.ylabel("Intensité")
+plt.title('Instantanés de l\'équation de Swift-Hohenberg\n r = {}, dt = {}, N = {}'.format(r,dt,N))
 plt.legend()
 plt.show()
 plt.clf()
 
 t_range = np.arange(0,t_max, dt)
 [xx,tt]=np.meshgrid(x_range,t_range)
-plt.contourf(xx,tt, U.T, cmap = "plama", levels = 50) # cmap = "jet" dans les consignes
+plt.contourf(xx,tt, U.T, cmap = "plasma", levels = 100) # cmap = "jet" dans les consignes
 plt.xlabel("x")
 plt.ylabel("t")
-plt.title("Évolution sous l'équation Swift-Hohenberg \n r = {}, dt = {}, N = {}".format(r,dt,N))
+plt.title("Simulation numérique de l'équation de Swift-Hohenberg \n r = {}, dt = {}, N = {}".format(r,dt,N))
 plt.colorbar()
 plt.show()
 
