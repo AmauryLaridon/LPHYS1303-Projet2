30f9fa56ac401eb15a3e35886fc367941f85f6fb
Amaury Laridon <amaurylaridon@protonmail.com>
2021-05-09 18:39:48 +0200

    Simulation cas intéret + noise

--- pattern.py
+++ pattern.py
@@ -9,11 +9,16 @@ from scipy.integrate import simps
 def u_0(x,L):
     gamma = 0.1
     return 1*(cos(2*pi*x/L) + gamma*cos(4*pi*x/L))
-    
+
 def noise_u_0(x,L):
-    gamma = 0.1
+    gamma = 0.5
     N = np.shape(x)[0]
     return 1*(cos(2*pi*x/L) + gamma*np.random.random(N)*cos(4*pi*x/L))
+
+def noise(x,L):
+    gamma = 0.5
+    N = np.shape(x)[0]
+    return gamma*np.random.random(N)
 
 def v_0(x,L):
     return (0.5 - 0.25*cos(2*pi*x/L))
@@ -23,8 +28,7 @@ def w_0(x,L):
 
 
 
-########################## Ex1 partie 1
-
+########################## Ex1 partie 1u
 def SH(f0,r,L):
     N = 1024
     dt = 0.05
@@ -34,11 +38,11 @@ def SH(f0,r,L):
 
     x_range = np.linspace(0, L-(L/N), N)
     k_range = np.arange(-N/2,N/2,1)
-    
+
     print("Résolution numérique avec une grille spatiale de {} points".format(N))
     print("Résolution numérique avec une grille temporelle de {} points".format(M))
     print("Paramètres numérique : L = {}, T = {}s, h = {}, k = {}, r = {}".format(L, T, h, dt, r))
-    
+
     fL_range = r - 1 + 2*((2*pi*k_range/L)**2) - ((2*pi*k_range/L)**4)
     """plt.plot(k_range,fL_range)
     plt.show()
@@ -51,7 +55,7 @@ def SH(f0,r,L):
     plt.legend()
     plt.show()
     plt.clf()"""
-    
+
     u_range = f0(x_range,L)
     U = np.zeros((N,M))
     U[:,0] = u_range
@@ -82,9 +86,9 @@ def SH(f0,r,L):
         ukc = (1/N)*np.fft.fftshift(np.fft.fft(u**3))
         DFTc[:,j] = ukc
 
-    return U
-            
-            
+    return U, x_range, [L, T, h, dt, r, T, M, N]
+
+
 
 
 """r = 0.2
@@ -132,11 +136,11 @@ def tl_mesure(f0,r,L):
 
     x_range = np.linspace(0, L-(L/N), N)
     k_range = np.arange(-N/2,N/2,1)
-    
+
     fL_range = r - 1 + 2*((2*pi*k_range/L)**2) - ((2*pi*k_range/L)**4)
     coef_1 = (1+(fL_range*dt/2))/(1-(fL_range*dt/2))
     coef_2 = dt/(1-(fL_range*dt/2))
-    
+
     u_range = f0(x_range,L)
     U = np.zeros((N,M))
     U[:,0] = u_range
@@ -156,7 +160,7 @@ def tl_mesure(f0,r,L):
     Départ = True
     t_pattern = 0
     lamb_f = 0
-    
+
     for j in range(1,M):
         d1 = DFT[:,j-1]
         dc1 = DFTc[:,j-1]
@@ -172,11 +176,11 @@ def tl_mesure(f0,r,L):
         U[:,j] = u
         ukc = (1/N)*np.fft.fftshift(np.fft.fft(u**3))
         DFTc[:,j] = ukc
-    
+
         # Temps d'apparition
         if not j%int(1/dt):
             continue
-    
+
         A_c = (1/L)*simps(abs(u), x_range)
         if A_c > seuil:
             if Départ :
@@ -186,7 +190,7 @@ def tl_mesure(f0,r,L):
                 # Longueur d'onde
                 k_f = uk.tolist().index(max(uk)) + N/2
                 lamb_f = 2*pi/k_f
-                
+
                 break
         else :
             if Départ :
@@ -201,27 +205,29 @@ def tl_mesure(f0,r,L):
 def rL_effect(u0, r_range, L_range):
     time = np.zeros((len(r_range), len(L_range)))
     wavelength = np.zeros((len(r_range), len(L_range)))
-    
+
     for n,r in enumerate(r_range):
         for m,L in enumerate(L_range):
             t_,l_ = tl_mesure(u0, r, L)
             time[n,m] = t_
             wavelength[n,m] = l_
-            
+
     plt.imshow(time)
     plt.xlabel("L = {}".format(L_range))
     plt.ylabel("r = {}".format(r_range))
     plt.colorbar()
     plt.show()
-    plt.clf()  
-      
+    plt.clf()
+
     plt.imshow(wavelength)
     plt.xlabel("L = {}".format(L_range))
     plt.ylabel("r = {}".format(r_range))
     plt.colorbar()
     plt.show()
-    plt.clf()    
-    
+    plt.clf()
+
+
+
     return time
 
 
@@ -245,11 +251,11 @@ def A_mesure(f0,r,L):
 
     x_range = np.linspace(0, L-(L/N), N)
     k_range = np.arange(-N/2,N/2,1)
-    
+
     fL_range = r - 1 + 2*((2*pi*k_range/L)**2) - ((2*pi*k_range/L)**4)
     coef_1 = (1+(fL_range*dt/2))/(1-(fL_range*dt/2))
     coef_2 = dt/(1-(fL_range*dt/2))
-    
+
     u_range = f0(x_range,L)
     U = np.zeros((N,M))
     U[:,0] = u_range
@@ -268,7 +274,7 @@ def A_mesure(f0,r,L):
     seuil = 0.2
     Départ = True
     t_pattern = 0
-    
+
     for j in range(1,M):
         d1 = DFT[:,j-1]
         dc1 = DFTc[:,j-1]
@@ -284,7 +290,7 @@ def A_mesure(f0,r,L):
         U[:,j] = u
         ukc = (1/N)*np.fft.fftshift(np.fft.fft(u**3))
         DFTc[:,j] = ukc
-    
+
     A = sqrt((1/L)*simps(U[:,-1]**2, x_range))
     return A
 
@@ -292,30 +298,55 @@ def A_mesure(f0,r,L):
 def r_bifurcation(u0, r_range):
     integ = np.zeros((len(r_range)))
     L = 100
-    
+
     for n,r in enumerate(r_range):
         A_ = A_mesure(u0, r, L)
         integ[n] = A_
-        
+
     plt.plot(r_range, integ)
     plt.xlabel("r")
     plt.ylabel("A")
     plt.show()
     plt.clf()
-    
-    
+
+
     integ_pos = [i for i in integ if i > 0.004]
     r_pos = r_range[-len(integ_pos):]
-    
+
     lr_pos = np.log(r_pos)
     lint_pos = np.log(integ_pos)
     plt.plot(lr_pos, lint_pos)
     plt.xlabel("ln(r)")
     plt.ylabel("ln(A)")
     plt.show()
-    plt.clf()  
+    plt.clf()
     print((lint_pos[-1]-lint_pos[0])/(lr_pos[-1] - lr_pos[0]))
-    
+
     return integ
 
-r_bifurcation(u_0, np.arange(0.035,0.045,0.001))
+#r_bifurcation(u_0, np.arange(0.035,0.045,0.001))
+#rL_effect(u_0, np.arange(-0.05,0.25,0.05), [25,50,100,150,200])
+time_ev = SH(noise, 0.2, 100)
+U = time_ev[0]
+x_range = time_ev[1]
+param = time_ev[2]
+
+plt.plot(x_range, U[:,0], label = "t = 0s")
+plt.plot(x_range, U[:,1], label = "t = {}s".format(param[3]))
+plt.plot(x_range, U[:,int(param[-2]/4)], label = "t = {:2.2f}s".format((param[-2]/4)*param[3]))
+plt.plot(x_range, U[:,-1], label = "t = {:2.2f}s".format(param[-3]))
+plt.xlabel("x")
+plt.ylabel("Intensité")
+plt.title('Instantanés de l\'équation de Swift-Hohenberg\n r = {}, dt = {}, N = {}, L = {}'.format(param[4],param[3],param[-1],param[0]))
+plt.legend()
+plt.show()
+plt.clf()
+
+t_range = np.arange(0,param[-3], param[3])
+[xx,tt]=np.meshgrid(x_range,t_range)
+plt.contourf(xx,tt, U.T, cmap = "plasma", levels = 100) # cmap = "jet" dans les consignes
+plt.xlabel("x")
+plt.ylabel("t")
+plt.title("Simulation numérique de l'équation de Swift-Hohenberg \n r = {}, dt = {}, N = {}, L = {}".format(param[4],param[3],param[-1],param[0]))
+plt.colorbar()
+plt.show()
